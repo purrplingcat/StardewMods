f345ffc8d9f8b2b0cb76ee63e0e96c0f537d218a
PurrplingCat <git@purrplingcat.com>
2020-07-05 19:14:41 +0200

    Initial commit

---
+++ update.sh
@@ -0,0 +1,8 @@
+cat mod.list | grep -e '^\w' | sed -e 's/^[[:space:]]*(.*)[[:space:]]*$/$1/' | while read -r repo; do
+   (
+      echo "> Updating repo $repo ...";
+      cd $repo;
+      git pull;
+   )
+done
+
