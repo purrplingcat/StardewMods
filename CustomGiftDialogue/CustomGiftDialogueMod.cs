cb6e024de4818dd9f90561dc756fe5335ffece2a
PurrplingCat <git@purrplingcat.com>
2020-12-18 12:48:26 +0100

    CGD: Fixed mess up with current dialogue replacement

--- CustomGiftDialogue/CustomGiftDialogueMod.cs
+++ CustomGiftDialogue/CustomGiftDialogueMod.cs
@@ -1,9 +1,8 @@
 ﻿using Harmony;
-using PurrplingCore.Dialogues;
 using StardewModdingAPI;
 using StardewValley;
+using StardewValley.Menus;
 using System;
-using System.Collections.Generic;
 using SObject = StardewValley.Object;
 
 namespace CustomGiftDialogue
@@ -12,6 +11,7 @@ namespace CustomGiftDialogue
     public class CustomGiftDialogueMod : Mod
     {
         internal static IMonitor ModMonitor { get; private set; }
+        internal static IReflectionHelper Reflection { get; private set; }
 
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
@@ -23,14 +23,17 @@ namespace CustomGiftDialogue
                 original: AccessTools.Method(typeof(NPC), nameof(NPC.receiveGift)),
                 postfix: new HarmonyMethod(this.GetType(), nameof(PATCH__After_receiveGift))
             );
+
+            Reflection = helper.Reflection;
         }
 
         private static void PATCH__After_receiveGift(NPC __instance, SObject o)
         {
             try
             {
-                if (FetchGiftReaction(__instance, o, out string giftDialogue))
+                if (GiftDialogueHelper.FetchGiftReaction(__instance, o, out string giftDialogue))
                 {
+                    GiftDialogueHelper.CancelCurrentDialogue(__instance);
                     Game1.drawDialogue(__instance, giftDialogue);
                 }
             }
@@ -39,30 +42,6 @@ namespace CustomGiftDialogue
                 ModMonitor.Log($"An error occurred during handle custom gift reaction dialogue: {ex.Message}", LogLevel.Error);
                 ModMonitor.Log(ex.ToString());
             }
-        }
-
-        /// <summary>
-        /// Try find a gift reaction dialogue text for gifted object to an NPC
-        /// </summary>
-        /// <param name="npc">NPC had recieved a gift</param>
-        /// <param name="obj">Gifted item recieved by this NPC</param>
-        /// <param name="dialogue">A reaction dialogue text for this gifted item to this NPC</param>
-        /// <returns>True if any gift reaction dialogue was found, otherwise false</returns>
-        private static bool FetchGiftReaction(NPC npc, SObject obj, out string dialogue)
-        {
-            string[] possibleKeys = new string[] { $"GiftReaction_{obj.Name.Replace(' ', '_')}", $"GiftReactionCategory_{obj.Category}" };
-
-            foreach (string dialogueKey in possibleKeys)
-            {
-                if (DialogueHelper.GetRawDialogue(npc.Dialogue, dialogueKey, out KeyValuePair<string, string> reaction))
-                {
-                    dialogue = reaction.Value;
-                    return true;
-                }
-            }
-
-            dialogue = "";
-            return false;
         }
     }
 }
---
+++ CustomGiftDialogue/GiftDialogueHelper.cs
@@ -0,0 +1,64 @@
+﻿using PurrplingCore.Dialogues;
+using StardewValley;
+using StardewValley.Menus;
+using System.Collections.Generic;
+using SObject = StardewValley.Object;
+
+namespace CustomGiftDialogue
+{
+    internal static class GiftDialogueHelper
+    {
+        /// <summary>
+        /// Try find a gift reaction dialogue text for gifted object to an NPC
+        /// </summary>
+        /// <param name="npc">NPC had recieved a gift</param>
+        /// <param name="obj">Gifted item recieved by this NPC</param>
+        /// <param name="dialogue">A reaction dialogue text for this gifted item to this NPC</param>
+        /// <returns>True if any gift reaction dialogue was found, otherwise false</returns>
+        public static bool FetchGiftReaction(NPC npc, SObject obj, out string dialogue)
+        {
+            string[] possibleKeys = new string[] { $"GiftReaction_{obj.Name.Replace(' ', '_')}", $"GiftReactionCategory_{obj.Category}" };
+
+            foreach (string dialogueKey in possibleKeys)
+            {
+                if (DialogueHelper.GetRawDialogue(npc.Dialogue, dialogueKey, out KeyValuePair<string, string> reaction))
+                {
+                    dialogue = reaction.Value;
+                    return true;
+                }
+            }
+
+            dialogue = "";
+            return false;
+        }
+
+        /// <summary>
+        /// Cancels current spoken dialogue for an NPC. 
+        /// DialogueBox for this dialogue will not shown.
+        /// </summary>
+        /// <param name="speaker">The NPC speaker</param>
+        public static void CancelCurrentDialogue(NPC speaker)
+        {
+            if (Game1.activeClickableMenu is DialogueBox dialogueBox && Game1.currentSpeaker == speaker)
+            {
+                // We wants override the default gift reaction dialogue
+                if (speaker.CurrentDialogue.Count > 0
+                    && GetDialogueBoxDialogue(dialogueBox) == speaker.CurrentDialogue.Peek())
+                {
+                    speaker.CurrentDialogue.Pop();
+                }
+
+                Game1.dialogueUp = false;
+                Game1.currentSpeaker = null;
+                Game1.activeClickableMenu = null;
+            }
+        }
+
+        private static Dialogue GetDialogueBoxDialogue(DialogueBox dialogueBox)
+        {
+            return CustomGiftDialogueMod.Reflection
+                .GetField<Dialogue>(dialogueBox, "characterDialogue")
+                .GetValue();
+        }
+    }
+}
