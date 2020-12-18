df31592fe6a51005844e63a76e2918fd3a2d44ca
PurrplingCat <git@purrplingcat.com>
2020-12-18 10:13:51 +0100

    CGD: Added support for gifted item category reaction

--- CustomGiftDialogue/CustomGiftDialogueMod.cs
+++ CustomGiftDialogue/CustomGiftDialogueMod.cs
@@ -1,7 +1,6 @@
 ﻿using Harmony;
 using PurrplingCore.Dialogues;
 using StardewModdingAPI;
-using StardewModdingAPI.Utilities;
 using StardewValley;
 using System;
 using System.Collections.Generic;
@@ -30,11 +29,9 @@ namespace CustomGiftDialogue
         {
             try
             {
-                string dialogueKey = $"GiftReaction_{o.Name.Replace(' ', '_')}";
-
-                if (DialogueHelper.GetRawDialogue(__instance.Dialogue, dialogueKey, out KeyValuePair<string, string> reaction))
+                if (FetchGiftReaction(__instance, o, out string giftDialogue))
                 {
-                    Game1.drawDialogue(__instance, reaction.Value);
+                    Game1.drawDialogue(__instance, giftDialogue);
                 }
             }
             catch (Exception ex)
@@ -42,6 +39,30 @@ namespace CustomGiftDialogue
                 ModMonitor.Log($"An error occurred during handle custom gift reaction dialogue: {ex.Message}", LogLevel.Error);
                 ModMonitor.Log(ex.ToString());
             }
+        }
+
+        /// <summary>
+        /// Try find a gift reaction dialogue text for gifted object to an NPC
+        /// </summary>
+        /// <param name="npc">NPC had recieved a gift</param>
+        /// <param name="obj">Gifted item recieved by this NPC</param>
+        /// <param name="dialogue">A reaction dialogue text for this gifted item to this NPC</param>
+        /// <returns>True if any gift reaction dialogue was found, otherwise false</returns>
+        private static bool FetchGiftReaction(NPC npc, SObject obj, out string dialogue)
+        {
+            string[] possibleKeys = new string[] { $"GiftReaction_{obj.Name.Replace(' ', '_')}", $"GiftReactionCategory_{obj.Category}" };
+
+            foreach (string dialogueKey in possibleKeys)
+            {
+                if (DialogueHelper.GetRawDialogue(npc.Dialogue, dialogueKey, out KeyValuePair<string, string> reaction))
+                {
+                    dialogue = reaction.Value;
+                    return true;
+                }
+            }
+
+            dialogue = "";
+            return false;
         }
     }
 }
