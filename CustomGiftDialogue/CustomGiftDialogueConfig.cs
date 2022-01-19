58ca2bde3d5052d8ccf511ca6e9c63963b9f377f
PurrplingCat <ellen@purrplingcat.com>
2022-01-19 19:30:38 +0100

    CDGU: reveal dialogues

--- CustomGiftDialogue/CustomGiftDialogueConfig.cs
+++ CustomGiftDialogue/CustomGiftDialogueConfig.cs
@@ -4,5 +4,7 @@
     {
         public bool CustomBirthdayDialogues = true;
         public bool CustomSecretSantaDialogues = true;
+        public double RevealDialogueChance = 0.025;
+        public int RevealDialogueMinHeartLevel = 1;
     }
 }
